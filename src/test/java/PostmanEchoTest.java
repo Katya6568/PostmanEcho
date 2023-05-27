02e51f6f3840c8be86c2c8c23d07797cfc341d20
Ekaterina Pavlova <sone4ka1414@gmail.com>
2023-05-27 13:17:07 +0300

    failed test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -6,17 +6,17 @@ public class PostmanEchoTest {
     @Test
     void PostmanEchoTest() {
         // Given - When - Then
-// Предусловия
+
         given()
                 .baseUri("https://postman-echo.com")
-                .body("clients data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .body("clients data")
+
                 .when()
                 .post("/post")
-// Проверки
+
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("clients data"))
+                .body("amount", equalTo("clients data"))
         ;
 
     }
